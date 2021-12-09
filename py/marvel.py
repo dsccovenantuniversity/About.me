abd31239b74a5b4bd3c4c83c0e5bb8a04f7d953b
Marvellous <ibironkemarv@gmail.com>
2021-12-09 15:10:51 +0100

    my commit

---
+++ py/marvel.py
@@ -0,0 +1,12 @@
+# wanna enter an infinite loop 
+# run this 
+infinite = True 
+Profile = "My name is Marvellous and I love Backend development "
+
+def About(Endless, Profile):
+    while Endless: 
+        print(Profile)
+
+
+About(infinite, Profile)
+
