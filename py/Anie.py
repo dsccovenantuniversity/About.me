b6ead75cf9251636fcd3aa54234c4302c76f1441
Anie <anie.emenike@gmail.com>
2022-05-07 07:53:31 +0100

    no stress

---
+++ py/Anie.py
@@ -0,0 +1,13 @@
+from unicodedata import name
+
+
+name= 'Anie'
+level = 100
+program = 'Computer Engineering'
+
+
+print('Hi, I am '+ name + '\nI am in ' + str(level) +' level\nStudying ' + program +'!')
+
+
+
+# Mic drop....walks away.....
